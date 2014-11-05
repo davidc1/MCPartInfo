c21ae969732c7cabfd6ddd114e30b429d92d212c
David <David Caratelli>
2014-11-05 11:02:26 -0600

    Calling some more functions in MCPartExample --David Caratelli

--- MCPartGetter/MCPartExample.cxx
+++ MCPartGetter/MCPartExample.cxx
@@ -105,6 +105,12 @@ namespace larlite {
 
       if (_MCgetter.searchParticleMap(result.at(j).getNodeIndex()) >= 0){
 	mcpart part = event_part->at(_MCgetter.searchParticleMap( result.at(j).getNodeIndex() ));
+	// Now get particle track
+	// Trajectory consisting only of start & end points
+	std::vector<std::vector<double> > partTrajStartEnd = _MCgetter.getStartEndTrajectory(&part);
+	std::vector<std::vector<double> > partTrajAll = _MCgetter.getTrajectoryPoints(&part);
+	std::vector<std::vector<double> > partTrajInTPC = _MCgetter.getTrajectoryPointsInTPC(&part,0); //0 cm buffer...up to TPC boundaries
+	std::vector<std::vector<double> > partTrajInTPCExtra = _MCgetter.getTrajectoryPointsInTPC(&part,100);//100 cm buffer...track up to 1 meter away from all directions
 	
 	if ( !(result.at(j).isPrimary()) ){
 	  if (_MCgetter.searchParticleMap(result.at(j).getParentId()) >= 0)
